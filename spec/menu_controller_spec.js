990dbd91ae0cc711283277188a6aa38ce15b1b7e
Brandon <brandoncrichie@gmail.com>
2018-12-18 12:39:47 -0800

    Checkpoint 4 Search Lists: Complete

--- spec/menu_controller_spec.js
+++ spec/menu_controller_spec.js
@@ -7,5 +7,27 @@ describe("MenuController", () => {
         this.menu = new MenuController();
     });
 
-
-});
+    
+   /* describe("#getContactCount()", () => {
+        
+            it("should return 0 when no contacts are in the book", () => {
+               expect(this.menu.getContactCount()).toBe(0);
+            });
+        
+            it("should return 1 when there is exactly one contact in the book", () => {
+               this.menu.book.addContact("Bob", "555-555-5555");
+               expect(this.menu.getContactCount()).toBe(1);
+            });
+     });
+    */
+    
+    
+    
+    
+    
+    describe("#remindMe()", () => {
+        it("should return that learning is life-long", () => {
+           expect(this.menu.remindMe()).toBe("Learning is a life-long pursuit");
+        });
+     });
+  });
